2e84eba032bb6bb37534ba6916cb04dcd745cc85
Jon Vik <jonvi039@student.liu.se>
2018-02-03 10:56:31 +0100

    add emoticon list and deng xiaoping feature

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings');
+const emoticons = require("./emoticons");
 
 client.on('ready',function () {
-    console.log('I\'m Online\nI\'m Online');
+    console.log('I\'m Online!');
 });
 
 client.on('message',function (message) {
-    if(message.content === 'ping'){
-        message.reply('pong');
+
+    if(message.author.id === client.user.id){return;}
+   // console.log(message.content);
+    switch (message.content.toLowerCase()){
+        case emoticons.said:
+            message.channel.send("baba äääääääääh");
+            break;
+        case "ping":
+            message.channel.send("pong");
+            break;
+        case "deng":
+            message.channel.send("Xiao...");
+            message.channel.send("PIIIIIINGG!!", {
+                file: "http://news.bbc.co.uk/media/images/38576000/jpg/_38576591_dengap238.jpg"
+            });
+            break;
+
+
     }
 });
 
